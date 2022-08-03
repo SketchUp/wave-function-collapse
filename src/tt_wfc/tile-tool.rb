4c3e2416ad53d4ce2a864409c5abf10c82fbac46
Thomas Thomassen <thomas@thomthom.net>
2022-08-03 17:08:27 +0200

    Basic tagging of connection points.

--- src/tt_wfc/tile-tool.rb
+++ src/tt_wfc/tile-tool.rb
@@ -79,18 +79,47 @@ module Examples
         view.invalidate
       end
 
+      # @param [Integer] flags
+      # @param [Integer] x
+      # @param [Integer] y
+      # @param [Sketchup::View] view
+      def getMenu(menu, flags, x, y, view)
+        unless @selection.empty?
+          menu.add_item('Assign Connection Type') do
+            prompt_assign_connection_type_to_selection
+          end
+          menu.add_separator
+        end
+        menu.add_item('Add Connection Type') do
+          prompt_add_connection_type
+        end
+        # menu.add_item('Remove Connection Type') do
+        #   prompt_remove_connection_type
+        # end
+        # menu.add_item('Edit Connection Type') do
+        #   prompt_edit_connection_type
+        # end
+      end
+
       # @param [Sketchup::View] view
       def draw(view)
         view.line_stipple = ''
 
         # Draw connections points.
-        points = []
-        @tiles.each { |tile|
-          points.concat(tile.connection_points)
-        }
+        # (Backgrounds)
+        points = @tiles.flat_map(&:connection_points)
         view.line_width = 2
         view.draw_points(points, 12, DRAW_FILLED_SQUARE, 'black')
-        view.draw_points(points, 10, DRAW_PLUS, 'white')
+        # (Cross-hairs)
+        connection_types = get_connection_types(view.model)
+        connection_colors = Hash[connection_types.map { |t| [t.type_id, t.color] }]
+        connections = @tiles.flat_map(&:connections)
+        connections.sort_by { |c| c.type || '' }.chunk { |c| c.type || '' }.each { |type_id, items|
+          color = connection_colors[type_id] || 'white'
+          pts = items.map(&:position)
+          view.draw_points(pts, 10, DRAW_PLUS, color)
+        }
+        # view.draw_points(points, 10, DRAW_PLUS, 'white')
 
         # Draw selected connection point.
         unless @selection.empty?
@@ -194,6 +223,66 @@ module Examples
           instance.layer = tile_tag
         }
         instances.map { |instance| Tile.new(instance) }
+      end
+
+      ATTR_DICT = 'tt_wfc'
+      ATTR_TYPES = 'connection_types'
+
+      ConnectionType = Struct.new(:type_id, :color)
+
+      # @param [Sketchup::Model] model
+      # @return [Array<ConnectionType>]
+      def get_connection_types(model)
+        model.get_attribute(ATTR_DICT, ATTR_TYPES, []).map { |data|
+          type_id, color = data
+          ConnectionType.new(type_id, color)
+        }
+      end
+
+      # @param [Sketchup::Model] model
+      # @param [ConnectionType] type
+      def add_connection_type(model, type)
+        raise unless type.is_a?(ConnectionType)
+        types = get_connection_types(model)
+        raise ArgumentError, "#{type[0]} already exist" if types.any? { |t| t[0] == type[0] }
+        types << type
+        model.set_attribute(ATTR_DICT, ATTR_TYPES, types.map(&:to_a))
+      end
+
+      def prompt_add_connection_type
+        prompts = ['Connection ID', 'Color']
+        defaults = ['connection-id', Sketchup::Color.names.sample]
+        list = ['', Sketchup::Color.names.join('|')]
+        input = UI.inputbox(prompts, defaults, list, 'Create Connection Type')
+        return unless input
+
+        type, color = input
+
+        model = Sketchup.active_model
+        model.start_operation('Add Connection Type', true)
+        # add_connection_type(model, [type, color])
+        add_connection_type(model, ConnectionType.new(type, color))
+        model.commit_operation
+      end
+
+      def prompt_assign_connection_type_to_selection
+        model = Sketchup.active_model
+        types = get_connection_types(model)
+        type_ids = types.map(&:first)
+
+        prompts = ['Connection ID']
+        defaults = [type_ids.sort.first || '']
+        list = [type_ids.sort.join('|')]
+        input = UI.inputbox(prompts, defaults, list, 'Assign Connection Type')
+        return unless input
+
+        type_id = input[0]
+        type = types.find { |t| t.type_id == type_id }
+        raise unless type
+
+        @selection.each { |connection|
+          connection.type = type_id
+        }
       end
 
     end # class
--- src/tt_wfc/tile.rb
+++ src/tt_wfc/tile.rb
@@ -5,46 +5,45 @@ module Examples
 
     class Tile
 
-      class RelationShips
-
-        attr_reader :connection_id, :connected
-
-        # @param [Symbol] connection_id
-        # @param [Array<Integer>] connected
-        def initialize(connection_id, connected)
-          @connection_id = connection_id
-          @connected = connected
-        end
-
-        def add(instance_id)
-          @connected << instance_id
-        end
-
-        def remove(instance_id)
-          @connected.delete(instance_id)
-        end
-
-      end # class
-
       class ConnectionPoint
 
         include BoundingBoxConstants
 
-        attr_reader :tile, :connection_id, :position, :relationships
+        attr_reader :tile, :connection_id, :position
+        attr_reader :type
 
         # @param [Tile] tile
         # @param [Symbol] connection_id
         # @param [Geom::Point3d] position
-        # @param [RelationShips] relationships
-        def initialize(tile, connection_id, position, relationships)
+        def initialize(tile, connection_id, position)
           @tile = tile
           @connection_id = connection_id
           @position = position
-          @relationships = relationships
+
+          @type = deserialize(tile.instance)
+        end
+
+        def type=(value)
+          @type = value
+          serialize(tile.instance)
         end
 
         def to_s
           "#{@tile}:#{@connection_id}"
+        end
+
+        private
+
+        SECTION_ID = 'tt_wfc'.freeze
+
+        # @param [Sketchup::ComponentInstance] instance
+        def serialize(instance)
+          instance.definition.set_attribute(SECTION_ID, @connection_id.to_s, @type)
+        end
+
+        # @param [Sketchup::ComponentInstance] instance
+        def deserialize(instance)
+          instance.definition.get_attribute(SECTION_ID, @connection_id.to_s, nil)
         end
 
       end
@@ -53,22 +52,11 @@ module Examples
         :north, :south, :east, :west
       ]
 
-      attr_reader :connections
+      attr_reader :instance
 
       # @param [Sketchup::ComponentInstance] instance
       def initialize(instance)
         @instance = instance
-        @connections = load_connections(instance)
-      end
-
-      # @param [Symbol] connection_id
-      # @param [Sketchup::ComponentInstance] other_instance
-      # @param [Symbol] other_connection_id
-      def connect(connection_id, other_instance, other_connection_id)
-        connect_to(connection_id, other_instance, other_connection_id)
-
-        other_tile = Tile.new(other_instance)
-        other_tile.connect_to(other_connection_id, @instance, connection_id)
       end
 
       # @return [Array<Geom::Point3d>]
@@ -82,8 +70,7 @@ module Examples
       def connections
         CONNECTION_IDS.map { |connection_id|
           pt = connection_position(@instance, connection_id)
-          relationships = @connections[connection_id]
-          ConnectionPoint.new(self, connection_id, pt, relationships)
+          ConnectionPoint.new(self, connection_id, pt)
         }
       end
 
@@ -92,54 +79,6 @@ module Examples
       end
 
       private
-
-      # @param [Symbol] connection_id
-      # @param [Sketchup::ComponentInstance] other_instance
-      # @param [Symbol] other_connection_id
-      def connect_to(connection_id, other_instance, other_connection_id)
-        check_connection_id(connection_id)
-        check_connection_id(other_connection_id)
-
-        relationships = @connections[connection_id]
-        relationships.add(other_instance.persistent_id)
-
-        write_connections_ids(@instance, connection_id, relationships.connected)
-        nil
-      end
-
-      # @param [Symbol] connection_id
-      # @raise [ArgumentError] if `connection_id` is not valid
-      def check_connection_id(connection_id)
-        raise ArgumentError, "invalid connection ID: #{connection_id}"
-      end
-
-      SECTION_ID = 'tt_wfc'.freeze
-
-      # @param [Sketchup::ComponentInstance] instance
-      # @return [Hash{Symbol, Connection}]
-      def load_connections(instance)
-        data = {}
-        CONNECTION_IDS.map { |connection_id|
-          # position = connection_position(instance, connection_id)
-          connections = read_connections_ids(instance, connection_id)
-          relationships = RelationShips.new(connection_id, connections)
-          data[connection_id] = relationships
-        }
-        data
-      end
-
-      # @param [Sketchup::ComponentInstance] instance
-      # @param [Symbol] connection_id
-      # @return [Array<Hash{Symbol, Integer}>]
-      def read_connections_ids(instance, connection_id)
-        instance.get_attribute(SECTION_ID, connection_id.to_s, [])
-      end
-
-      # @param [Sketchup::ComponentInstance] instance
-      # @param [Array<Hash{Symbol, Integer}>] connections
-      def write_connections_ids(instance, connection_id, connections)
-        instance.set_attribute(SECTION_ID, connection_id.to_s, connections)
-      end
 
       # @param [Sketchup::ComponentInstance] instance
       # @param [Symbol] connection_id
