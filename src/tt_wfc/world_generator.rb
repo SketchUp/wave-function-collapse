c298ba3b1458b8179b3c3758894b180f7155af06
Thomas Thomassen <thomas@thomthom.net>
2022-08-04 13:30:08 +0200

    Fix placeholder tile generation.

--- src/tt_wfc/world_generator.rb
+++ src/tt_wfc/world_generator.rb
@@ -60,10 +60,10 @@ module Examples
       def generate_placeholder(model)
         definition = model.definitions.add('WFC Placeholder')
         points = [
-          Geom::Point3d.new(0, 0, 0),
-          Geom::Point3d.new(1.0.m, 0, 0),
-          Geom::Point3d.new(1.0.m, 1.0.m, 0),
-          Geom::Point3d.new(0, 1.0.m, 0),
+          Geom::Point3d.new(-0.5.m, -0.5.m, 0),
+          Geom::Point3d.new( 0.5.m, -0.5.m, 0),
+          Geom::Point3d.new( 0.5.m,  0.5.m, 0),
+          Geom::Point3d.new(-0.5.m,  0.5.m, 0),
         ]
         face = definition.entities.add_face(points)
         face.reverse! if face.normal.samedirection?(Z_AXIS.reverse)
@@ -76,13 +76,15 @@ module Examples
       # @param [Sketchup::ComponentDefinition] placeholder
       # @return [Array<Sketchup::ComponentInstance>]
       def generate_instance_grid(model, placeholder)
-        offset = 500.mm
+        size = 1.m
+        offset = size / 2
+
         instances = []
         group = model.entities.add_group
         group.transform!(Geom::Vector3d.new(0, 0, 1.m))
         width.times { |x|
           height.times { |y|
-            point = Geom::Point3d.new((x+1) * offset, (y+1) * offset, 0)
+            point = Geom::Point3d.new((x * size) + offset, (y * size) + offset, 0)
             tr = Geom::Transformation.translation(point)
             instances << group.entities.add_instance(placeholder, tr)
           }
